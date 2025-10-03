443acde572c78ecea56e10b8dff9cc0cb60d9879
Joice Conte <joiceconte@gmail.com>
2025-10-03 23:49:26 +0200

    updating page

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -7,17 +7,19 @@ export default async function isAuth(req, res, next) {
   try {
     // procura o token no header
     const authHeader = req.headers.authorization;
+
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res.status(401).json({ error: "No token provided" });
     }
 
     const token = authHeader.split(" ")[1];
-
+    console.log("📌 Token recebido:", token);
     // verifica token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     // busca o usuário no banco (sem senha)
     const user = await User.findById(decoded.id).select("-password");
+
     if (!user) {
       return res.status(401).json({ error: "User not found" });
     }
@@ -26,7 +28,6 @@ export default async function isAuth(req, res, next) {
     req.user = user;
     next();
   } catch (err) {
-    console.error("isAuth error:", err.message);
     return res.status(401).json({ error: "Invalid or expired token" });
   }
 }
